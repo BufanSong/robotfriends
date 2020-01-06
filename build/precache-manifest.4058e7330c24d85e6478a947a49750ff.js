6814a3977d3b93f28ab5021682bccbe876f519b9
Bufan Song <bs3498@nyu.edu>
2020-01-06 15:16:10 -0800

    update

---
+++ build/precache-manifest.4058e7330c24d85e6478a947a49750ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1f0235388aa0d6851aad629657f5d5",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "449890cabe2ea1118d2d",
+    "url": "/robotfriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "44566b38cee35238b807",
+    "url": "/robotfriends/static/css/main.a6437814.chunk.css"
+  },
+  {
+    "revision": "449890cabe2ea1118d2d",
+    "url": "/robotfriends/static/js/2.f0d44eb8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robotfriends/static/js/2.f0d44eb8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44566b38cee35238b807",
+    "url": "/robotfriends/static/js/main.5852b47a.chunk.js"
+  },
+  {
+    "revision": "c9b589edb167e4ed9da1",
+    "url": "/robotfriends/static/js/runtime-main.a78a2d9d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c255dfdf6508f5426ec7db8c0b42081.js"
+  "/robotfriends/precache-manifest.4058e7330c24d85e6478a947a49750ff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robotfriends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
