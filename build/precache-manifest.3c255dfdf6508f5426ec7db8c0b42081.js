cf3842e5614944a14695f086e702af66fea84630
Bufan Song <bs3498@nyu.edu>
2020-01-06 14:38:36 -0800

    project added

---
+++ build/precache-manifest.3c255dfdf6508f5426ec7db8c0b42081.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f990026a9ce4a67bb9654c23d1799be3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8c242a663d4a617c776",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d0171cf01c6169128488",
+    "url": "/static/css/main.fd5fb942.chunk.css"
+  },
+  {
+    "revision": "b8c242a663d4a617c776",
+    "url": "/static/js/2.c7991b8a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c7991b8a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0171cf01c6169128488",
+    "url": "/static/js/main.9b31d103.chunk.js"
+  },
+  {
+    "revision": "aa6fed8716c2be098a58",
+    "url": "/static/js/runtime-main.55a556ca.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
